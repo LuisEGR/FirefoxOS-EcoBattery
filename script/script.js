61bbb9ad5e3355a81d1f2ebc91918bcc3952f17e
Luis Enrique Gonzalez Rodriguez <luisegr149@gmail.com>
2013-08-24 21:51:09 -0500

    Todos los archivos necesarios

---
+++ script/script.js
@@ -0,0 +1,59 @@
+$(window).resize(function(){
+
+    $('.spl').css({
+        position:'absolute',
+        left: ($(window).width() - $('.className').outerWidth())/2,
+        top: ($(window).height() - $('.className').outerHeight())/2
+    });
+
+});
+ $(window).resize();
+$(document).ready(function() {
+
+   $(window).resize();
+
+audio = document.getElementById('snd'); 
+
+  //dom.battery.enabled = true;
+var bateria = window.navigator.battery;
+var crgndo;
+var imagen ;
+var carga = bateria.level *100;
+$('#porcentaje').html(carga +"%");
+           if(bateria.charging){
+                  $('#info').html("Cargando");
+                  $('#char').fadeIn(400);
+                }else{
+                  $('#info').html("Batería");
+                  $('#char').fadeOut(400);
+                }       
+                
+//battery.dischargingTime
+  setInterval(function ()  {                
+                var carga = bateria.level *100;
+                if(carga == 100 && bateria.charging){
+                   navigator.vibrate(1000);
+                   audio.currentTime=0;
+                  audio.play(); 
+                  alert("La Batería esta llena, desconecte el dispositivo para ahorrar energía");
+                }                   
+                if(bateria.charging){
+                  $('#info').html("Cargando");
+                  $('#char').fadeIn(400);
+                }else{
+                  $('#info').html("Batería");
+                  $('#char').fadeOut(400);
+                }              
+                if(carga >= 80){
+                  imagen = "Battery Full.png";
+                }else if(carga < 80 && carga >= 50){
+                  imagen = "Battery Medium.png";
+                }else if(carga < 50 && carga >=10){
+                  imagen = "Critical Energy.png";
+                }else{
+                    imagen = "Battery Empty.png";
+                }
+                $('#imgbat').attr("src",imagen);
+                $('#porcentaje').html(carga +"%");
+      }, 5000);
+});
